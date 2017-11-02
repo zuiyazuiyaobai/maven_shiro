977b542ed2ff80f6faf960698d50ba853169cf37
changdeveloper <985395955@qq.com>
2017-11-02 15:44:41 +0800

    upload

---
+++ src/main/java/com/wotong/commons/ueditor/upload/Uploader.java
@@ -0,0 +1,28 @@
+package com.wotong.commons.ueditor.upload;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.wotong.commons.ueditor.ActionConfig;
+import com.wotong.commons.ueditor.define.State;
+import com.wotong.commons.ueditor.manager.IUeditorFileManager;
+
+public class Uploader {
+	private HttpServletRequest request = null;
+	private ActionConfig conf = null;
+
+	public Uploader(HttpServletRequest request, ActionConfig conf) {
+		this.request = request;
+		this.conf = conf;
+	}
+
+	public final State doExec(IUeditorFileManager fileManager) {
+		String filedName = conf.getFieldName();
+		State state = null;
+		if (conf.isBase64()) {
+			state = Base64Uploader.save(fileManager, request.getParameter(filedName), conf);
+		} else {
+			state = BinaryUploader.save(fileManager, request, conf);
+		}
+		return state;
+	}
+}
